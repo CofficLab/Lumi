da9b632ba971aeab4b52480d23de4456954d8972
nookery <nooks@qq.com>
2026-03-13 18:39:52 +0800

    chore(llm): enable verbose logging in LLMAPIService

--- LumiApp/Core/Services/LLM/LLMAPIService.swift
+++ LumiApp/Core/Services/LLM/LLMAPIService.swift
@@ -8,7 +8,7 @@ import MagicKit
 /// 此类可以在后台线程执行
 class LLMAPIService: SuperLog, @unchecked Sendable {
     nonisolated static let emoji = "🌐"
-    nonisolated static let verbose = false
+    nonisolated static let verbose = true
     
     /// URLSession 配置
     private nonisolated let session: URLSession
