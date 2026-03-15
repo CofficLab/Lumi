ffa9784785ff4dd93620e48198b28a160ab8a5ca
Nookery <nooks@qq.com>
2026-03-15 19:48:23 +0800

    refactor(settings): restructure provider settings into local and remote views
    - Split ProviderSettingsView into LocalProviderSettingsView and RemoteProviderSettingsView
    - Reorganize settings UI components into Local/ and Remote/ directories
    - Remove deprecated ProviderSettings directory
    - Update localization strings for cloud and local LLM providers
    - Improve model management UI with separate sections for local and remote models

--- LumiApp/Core/Views/Settings/SettingView.swift
+++ LumiApp/Core/Views/Settings/SettingView.swift
@@ -19,7 +19,8 @@ struct SettingView: View {
     enum SettingTab: String, CaseIterable, Hashable {
         case general = "通用"
         case theme = "主题"
-        case provider = "供应商"
+        case localProvider = "本地供应商"
+        case remoteProvider = "云端供应商"
         case plugins = "插件管理"
         case about = "关于"
 
@@ -27,7 +28,8 @@ struct SettingView: View {
             switch self {
             case .general: return "gearshape"
             case .theme: return "paintbrush.fill"
-            case .provider: return "cpu"
+            case .localProvider: return "cpu"
+            case .remoteProvider: return "network"
             case .plugins: return "puzzlepiece.extension"
             case .about: return "info.circle"
             }
@@ -130,8 +132,10 @@ struct SettingView: View {
                             GeneralSettingView()
                         case .theme:
                             ThemeSettingView()
-                        case .provider:
-                            ProviderSettingsView()
+                        case .localProvider:
+                            LocalProviderSettingsView()
+                        case .remoteProvider:
+                            RemoteProviderSettingsView()
                         case .plugins:
                             PluginSettingsView()
                         case .about:
