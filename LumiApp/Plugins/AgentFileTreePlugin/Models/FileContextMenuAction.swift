21c0e9e0ffe84feab1a595069bf31e14ded28926
Nookery <nooks@qq.com>
2026-04-10 21:30:56 +0800

    refactor: reorganize FileNodeView with Event Handler block and extract file operations to service
    - Add Event Handler extension for click, context menu, refresh token, and drag handlers
    - Remove all print statements for cleaner production code
    - Extract openInTerminal and openInVSCode to ProjectTreeFileService for reusability
    - Add copyPath and copyRelativePath utilities
    - Add EnvironmentObject for projectVM access

---
+++ LumiApp/Plugins/AgentFileTreePlugin/Models/FileContextMenuAction.swift
@@ -0,0 +1,14 @@
+// MARK: - File Context Menu Action
+
+/// 文件节点右键菜单操作类型
+enum FileContextMenuAction {
+    case newFile
+    case newFolder
+    case rename
+    case revealInFinder
+    case openInVSCode
+    case openInTerminal
+    case delete
+    case copyPath
+    case copyRelativePath
+}
