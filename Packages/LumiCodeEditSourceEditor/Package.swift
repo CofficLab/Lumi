19e0083abde8f143fc42467cf49bdcc9145f6507
nookery <nooks@qq.com>
2026-05-22 10:30:46 +0800

    chore(CodeEdit): trim vendored source editor package

--- Packages/LumiCodeEditSourceEditor/Package.swift
+++ Packages/LumiCodeEditSourceEditor/Package.swift
@@ -30,8 +30,7 @@ let package = Package(
         .package(
             url: "https://github.com/ChimeHQ/TextFormation",
             from: "0.8.2"
-        ),
-        .package(url: "https://github.com/pointfreeco/swift-custom-dump", from: "1.0.0")
+        )
     ],
     targets: [
         // A source editor with useful features for code editing.
@@ -42,16 +41,6 @@ let package = Package(
                 "CodeEditLanguages",
                 "TextFormation",
                 .product(name: "CodeEditSymbols", package: "LumiCodeEditSymbols")
-            ]
-        ),
-
-        // Tests for the source editor
-        .testTarget(
-            name: "CodeEditSourceEditorTests",
-            dependencies: [
-                "CodeEditSourceEditor",
-                "CodeEditLanguages",
-                .product(name: "CustomDump", package: "swift-custom-dump")
             ]
         ),
     ]
