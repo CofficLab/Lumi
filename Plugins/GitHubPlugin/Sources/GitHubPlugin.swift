02c7961f114d2e03f4d0c480114c29eef3d14b3a
Nookery <nooks@qq.com>
2026-06-11 21:49:43 +0800

    feat(github): enable GitHubPlugin permanently

--- Plugins/GitHubPlugin/Sources/GitHubPlugin.swift
+++ Plugins/GitHubPlugin/Sources/GitHubPlugin.swift
@@ -6,7 +6,7 @@ import SwiftUI
 
 /// GitHub 插件：合并了 CLI 检测和生态洞察功能。
 public enum GitHubPlugin: LumiPlugin {
-    public static let policy: LumiPluginPolicy = .disabled
+    public static let policy: LumiPluginPolicy = .alwaysOn
     public static let category: LumiPluginCategory = .development
     public static let iconName = "network"
 
