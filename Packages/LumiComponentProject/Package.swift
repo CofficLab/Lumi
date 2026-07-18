4500c449392fef85272349e93d9ca25cbb7e2867
Nookery <nooks@qq.com>
2026-07-18 16:04:05 +0800

    feat: add LumiComponentProject package

---
+++ Packages/LumiComponentProject/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "LumiComponentProject",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "LumiComponentProject",
+            targets: ["LumiComponentProject"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "../SuperLogKit"),
+    ],
+    targets: [
+        .target(
+            name: "LumiComponentProject",
+            dependencies: [
+                .product(name: "SuperLogKit", package: "SuperLogKit"),
+            ],
+            path: "Sources"
+        ),
+    ]
+)
