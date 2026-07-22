c718ec83e827ace22a72af861fa1ee6eed37e8b5
nookery <nooks@qq.com>
2026-07-22 17:31:33 +0800

    refactor: update StatusBar, Settings, Logo, ViewContainer plugins

--- Plugins/ViewContainerPlugin/Sources/ViewContainerPlugin/ViewContainerPlugin.swift
+++ Plugins/ViewContainerPlugin/Sources/ViewContainerPlugin/ViewContainerPlugin.swift
@@ -31,7 +31,7 @@ public static let policy: LumiPluginPolicy = .disabled  // 核心插件，优先
 
     // MARK: - LumiPlugin
 
-    public func register(kernel: LumiKernel) throws {
+    public func onReady(kernel: LumiKernel) throws {
         // 1. 注册 ViewContainerService（内核服务）
         let viewContainerServiceInstance = DefaultViewContainerProviding()
         kernel.registerViewContainerService(viewContainerServiceInstance)
