ce6ba7985f2e2c827209d2f3ff12979ae317b4fc
nookery <nooks@qq.com>
2026-07-27 11:27:54 +0800

    feat(memory): register memory tools in plugin

--- Plugins/MemoryPlugin/Sources/MemoryPlugin.swift
+++ Plugins/MemoryPlugin/Sources/MemoryPlugin.swift
@@ -50,6 +50,14 @@ public final class MemoryPlugin: LumiPlugin, SuperLog {
 
     public func llmProviders(kernel: LumiKernel) -> [any LumiLLMProvider] { [] }
     public func subAgents(kernel: LumiKernel) -> [LumiSubAgentDefinition] { [] }
+    public func agentTools(kernel: LumiKernel) -> [any LumiAgentTool] {
+        [
+            SaveMemoryTool(),
+            ListMemoriesTool(),
+            RecallMemoryTool(),
+            DeleteMemoryTool(),
+        ]
+    }
     public func messageRenderers(kernel: LumiKernel) -> [LumiMessageRendererItem] { [] }
     public func menuBarContentItems(kernel: LumiKernel) -> [LumiMenuBarContentItem] { [] }
     public func menuBarPopupItems(kernel: LumiKernel) -> [LumiMenuBarPopupItem] { [] }
