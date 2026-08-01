9a4ac0f3fc3b02a5d4c327665d9fb23a66cf1fb1
Nookery <nooks@qq.com>
2026-08-01 10:04:10 +0800

    refactor(VerbosityPlugin): move ToolbarMetrics into Model/ subdirectory
    Move ToolbarMetrics.swift from Sources/ into Sources/Model/ to match the
    directory layout used by other VerbosityPlugin source files.

---
+++ Plugins/VerbosityPlugin/Sources/Model/ToolbarMetrics.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+enum ToolbarMetrics {
+    static let chipIconSize: CGFloat = 10
+    static let chipTextSize: CGFloat = 10
+    static let chipTextWeight: Font.Weight = .medium
+    static let chipSpacing: CGFloat = 3
+    static let chipHorizontalPadding: CGFloat = 6
+    static let chipVerticalPadding: CGFloat = 3
+    static let chipCornerRadius: CGFloat = 5
+}
