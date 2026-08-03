113c8269e0979d7b0c713a178bfac3418b62a094
nookery <nooks@qq.com>
2026-08-03 15:55:56 +0800

    refactor(DownloadPlugin): update plugin implementation

--- Plugins/DownloadPlugin/Sources/DownloadPlugin.swift
+++ Plugins/DownloadPlugin/Sources/DownloadPlugin.swift
@@ -18,7 +18,7 @@ public final class DownloadPlugin: LumiPlugin, SuperLog {
     public let order = 92
     public let policy: LumiPluginPolicy = .alwaysOn
     public let category: LumiPluginCategory = .agent
-    public let stage: LumiPluginStage = .stable
+    public let stage: LumiPluginStage = .beta
     public let pluginDescription = "Download files with progress tracking and batch support."
 
     public init() {}
