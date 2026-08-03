b021253d0be67c2a208d5c99d084191d29cab6e6
nookery <nooks@qq.com>
2026-08-03 16:59:21 +0800

    refactor(TerminalPlugin): update plugin implementation

--- Plugins/TerminalPlugin/Sources/TerminalPlugin.swift
+++ Plugins/TerminalPlugin/Sources/TerminalPlugin.swift
@@ -9,7 +9,9 @@ public final class TerminalPlugin: LumiPlugin {
     nonisolated(unsafe) private var pluginsChangedObserver: NSObjectProtocol?
 
     public let id = "com.coffic.lumi.plugin.terminal"
-    public let name = "Terminal"
+    public var name: String {
+        LumiPluginLocalization.string("Terminal", bundle: .module)
+    }
     public let order = 279
 	public let policy: LumiPluginPolicy = .optIn
 
