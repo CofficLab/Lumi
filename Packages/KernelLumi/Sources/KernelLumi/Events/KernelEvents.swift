45afe4406109da2b9251241d964a2cf84d120f32
nookery <nooks@qq.com>
2026-08-14 16:58:38 +0800

    feat: add conversationDidCreate event for precise new conversation notifications

--- Packages/KernelLumi/Sources/KernelLumi/Events/KernelEvents.swift
+++ Packages/KernelLumi/Sources/KernelLumi/Events/KernelEvents.swift
@@ -9,6 +9,7 @@ public enum KernelLumiEvent: String, CaseIterable, Sendable {
     case enabledPluginsDidChange = "com.coffic.lumi.enabledPluginsDidChange"
     case messagesDidChange = "com.coffic.lumi.messagesDidChange"
     case conversationsDidChange = "com.coffic.lumi.conversationsDidChange"
+    case conversationDidCreate = "com.coffic.lumi.conversationDidCreate"
     case selectedConversationDidChange = "com.coffic.lumi.selectedConversationDidChange"
     case conversationTitleDidChange = "com.coffic.lumi.conversationTitleDidChange"
     case conversationDidDelete = "com.coffic.lumi.conversationDidDelete"
