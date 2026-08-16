4c896df39b9453b1d2960e3453517d2569a18007
Nookery <nooks@qq.com>
2026-08-16 10:35:17 +0800

    chore(ProviderActivityBar): add LumiUI dependency

--- Packages/ProviderActivityBar/Package.swift
+++ Packages/ProviderActivityBar/Package.swift
@@ -15,10 +15,14 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(path: "../LumiUI"),
     ],
     targets: [
         .target(
             name: "ProviderActivityBar",
+            dependencies: [
+                .product(name: "LumiUI", package: "LumiUI"),
+            ],
             path: "Sources/ProviderActivityBar"
         ),
         .testTarget(
