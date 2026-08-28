34ebf6f57f770def739c3cc5f2f9116cb487d972
nookery <nooks@qq.com>
2026-08-28 11:16:29 +0800

    feat(PluginStorage): conform to SuperLog protocol and add KitSuperLog dependency

--- Packages/PluginStorage/Package.swift
+++ Packages/PluginStorage/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     platforms: [.macOS(.v14)],
     products: [.library(name: "PluginStorage", targets: ["PluginStorage"])],
     dependencies: [
+        .package(path: "../KitSuperLog"),
         .package(path: "../KernelCore"),
         .package(path: "../ProviderStorage"),
     ],
@@ -14,6 +15,7 @@ let package = Package(
         .target(
             name: "PluginStorage",
             dependencies: [
+                "KitSuperLog",
                 .product(name: "KernelCore", package: "KernelCore"),
                 .product(name: "ProviderStorage", package: "ProviderStorage"),
             ],
