3e910911173f292d1c625aae1369f27604955ab2
Nookery <nooks@qq.com>
2026-08-29 14:56:57 +0800

    refactor(PluginAgentTempStorage): move resources to package root

--- Packages/PluginAgentTempStorage/Package.swift
+++ Packages/PluginAgentTempStorage/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
                 "ProviderToolManager",
                 "KitSuperLog",
             ],
-            resources: [.process("Resources/Localizable.xcstrings")]
+            resources: [.process("../../Resources/Localizable.xcstrings")]
         ),
     ]
 )
