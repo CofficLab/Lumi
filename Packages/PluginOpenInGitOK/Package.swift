38f9ca43a5f33a9e95e1d01d9f7790c2b520ff63
Nookery <nooks@qq.com>
2026-09-12 19:37:22 +0800

    test(plugin-open-in-gitok): cover lifecycle registration

--- Packages/PluginOpenInGitOK/Package.swift
+++ Packages/PluginOpenInGitOK/Package.swift
@@ -32,5 +32,15 @@ let package = Package(
             path: "Sources/PluginOpenInGitOK",
             resources: [.process("../../Resources/Localizable.xcstrings")]
         ),
+        .testTarget(
+            name: "PluginOpenInGitOKTests",
+            dependencies: [
+                "PluginOpenInGitOK",
+                .product(name: "KernelCore", package: "KernelCore"),
+                .product(name: "OpenInKit", package: "OpenInKit"),
+                .product(name: "ProviderDocsView", package: "ProviderDocsView"),
+                .product(name: "ProviderToolManager", package: "ProviderToolManager"),
+            ]
+        ),
     ]
 )
